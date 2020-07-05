7f5cf954b20dfd4532edc60a4d7adb56b238de08
Praise T. Okonta <opraiz6@yahoo.com>
2020-07-05 20:31:08 +0100

    Add files via upload
    files commit

---
+++ helpers/db_client.ts
@@ -0,0 +1,14 @@
+import { MongoClient, Database } from "https://deno.land/x/mongo@v0.8.0/mod.ts";
+
+let db: Database
+
+export function connect() {
+    const client = new MongoClient();
+    client.connectWithUri("")
+
+    db = client.database('todo-app')
+}
+
+export function getDb() {
+    return db
+}
